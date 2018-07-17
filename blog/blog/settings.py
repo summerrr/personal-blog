32cd3dc113cc06ffaa78ccc19fa7b7bfa7eecc30
summerrr <838660384@qq.com>
2018-07-17 22:12:28 +0800

    部署配置

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -137,3 +137,22 @@ STATIC_URL = '/static/'
 BOOTSTRAP3={
     'include_jquery':True,
 }
+#heroku设置
+cwd=os.getcwd()#获取当前的工作目录
+#确保这个设置文件在本地和在线都能使用，只有部署到kuroku才会执行if
+if cwd=='/app' or cwd[:4]=='/tmp':
+    import dj_database_url
+    DATABASES={
+        'default': dj_database_url.config(default='postgres://localhost ')
+    } 
+    
+    SECURE_PROXY_SSL_HEADER=('HTTP_X_FORWARDED_PROTO','https')
+
+    ALLOWED_HOSTS=['*']#支持所有的主机头
+    #静态资产配置
+    BASE_DIR=os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT='staticfiles'
+	STATIC_URL = '/static/'
+    STATICFILES_DIRS=(
+        os.path.join(BASE_DIR,'static'),
+    )
